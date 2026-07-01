5e8e09a0dfe5bff6893a07cbb39728cac3bf88b0
fabellianurhasna-dotcom <fabellianurhasnaalyasusanto@students.amikom.ac.id>
2026-07-02 00:21:14 +0700

    tugas 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,11 +10,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-    $middleware->validateCsrfTokens(except: [
-        'admin/midtrans/callback',
-    ]);
-})
+    ->withMiddleware(function (Middleware $middleware): void {
+        // Mencegah Laravel memblokir request masuk dari Midtrans karena tidak membawa CSRF token
+        $middleware->validateCsrfTokens(except: [
+            'adminmidtrans/callback',
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
