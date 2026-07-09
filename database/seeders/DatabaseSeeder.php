b5ccee90f9be785a0c0cf258663c7a6cfcd8b9a8
fabellianurhasna-dotcom <fabellianurhasnaalyasusanto@students.amikom.ac.id>
2026-07-09 22:17:41 +0700

    benerin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,33 +29,37 @@ class DatabaseSeeder extends Seeder
         );
 
         // 2. Insert Kategori Event
-        $category = Category::create([
-            'name' => 'Seminar IT',
-            'slug' => 'seminar-it',
-        ]);
+        $category = Category::firstOrCreate(
+            ['slug' => 'seminar-it'],
+            ['name' => 'Seminar IT']
+        );
 
         // 3. Insert Event 1
-        Event::create([
-            'category_id' => $category->id,
-            'title' => 'Judul Event Inkubator',
-            'description' => 'Deskripsi untuk event inkubator.',
-            'date' => '2026-04-30 09:00:00',
-            'location' => 'Inkubator Amikom',
-            'price' => 50000,
-            'stock' => 100,
-            'poster_path' => 'posters/event-2.png',
-        ]);
+        Event::firstOrCreate(
+            ['title' => 'Judul Event Inkubator'],
+            [
+                'category_id' => $category->id,
+                'description' => 'Deskripsi untuk event inkubator.',
+                'date' => '2026-04-30 09:00:00',
+                'location' => 'Inkubator Amikom',
+                'price' => 50000,
+                'stock' => 100,
+                'poster_path' => 'posters/event-2.png',
+            ]
+        );
 
         // 4. Insert Event 2
-        Event::create([
-            'category_id' => $category->id,
-            'title' => 'AI & FUTURE TECH SUMMIT 2026',
-            'description' => 'Jelajahi tren terkini dalam kecerdasan buatan dan teknologi masa depan bersama para ahli di bidangnya.',
-            'date' => '2026-05-01 13:00:00',
-            'location' => 'Cinema Unit 6',
-            'price' => 50000,
-            'stock' => 100,
-            'poster_path' => 'posters/event-3.png',
-        ]);
+        Event::firstOrCreate(
+            ['title' => 'AI & FUTURE TECH SUMMIT 2026'],
+            [
+                'category_id' => $category->id,
+                'description' => 'Jelajahi tren terkini dalam kecerdasan buatan dan teknologi masa depan bersama para ahli di bidangnya.',
+                'date' => '2026-05-01 13:00:00',
+                'location' => 'Cinema Unit 6',
+                'price' => 50000,
+                'stock' => 100,
+                'poster_path' => 'posters/event-3.png',
+            ]
+        );
     }
 }
